a7f1fa03b3be711365933f36d748030f272dbabd
Jiří Jehlář <33741898+Herman1994@users.noreply.github.com>
2021-01-07 18:23:04 +0100

    Základ

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+        <head>
+            <title>Model Školy</title>
+            <meta charset="UTF-8">
+
+        </head>
+    <body>
+<?php
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/body.css">
+<link rel="stylesheet" type="text/js" href="assets/body.js">
+<div class="topnav">
+  <a href="#home" class="active">Logo</a>
+
+  <div id="myLinks">
+    <a href="#project">O Projetu</a>
+    <a href="#about">O mě</a>
+  </div>
+
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+?>
+</body>
