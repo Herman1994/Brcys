02a56e86cc9558a53b2f115602f19e49a2d6a1cc
Jiří Jehlář <33741898+Herman1994@users.noreply.github.com>
2021-01-07 18:37:46 +0100

    oprava

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <meta charset="UTF-8">
 
         </head>
-    <body>
+<body>
 <?php
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="assets/body.css">
@@ -13,14 +13,17 @@
 <div class="topnav">
   <a href="#home" class="active">Logo</a>
 
-  <div id="myLinks">
-    <a href="#project">O Projetu</a>
-    <a href="#about">O mě</a>
-  </div>
-
+        <div id="myLinks">
+            <a href="#project">O Projetu</a>
+            <a href="#about">O mě</a>
+        </div>
+            <div id="section">
+                <br>Nejake sracky bla bla bla</br>
+            </div>    
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
 </div>
 ?>
 </body>
+</html>
